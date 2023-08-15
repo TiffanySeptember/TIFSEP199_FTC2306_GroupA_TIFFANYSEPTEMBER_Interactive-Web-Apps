a04dd0ccf04a1757695da47997fd99e8710fbc3e
TiffanySeptember <137154346+TiffanySeptember@users.noreply.github.com>
2023-08-15 13:08:52 +0200

    added details

--- IWA7/challenge3.js
+++ IWA7/challenge3.js
@@ -10,29 +10,32 @@ const divider = "----------------------------------";
 
 // Only change below this line
 
-const owed = parseFloat(leoBalance) + parseFloat(sarahBalance);
+const owed = parseFloat(leoBalance) + parseFloat(sarahBalance); //This line calculates the total amount owed by adding the parsed floating-point values
 
 function formatNumber(number) {
-  const [integerPart, decimalPart] = number.toFixed(2).split(".");
+  //function definition that takes a number as an argument and returns a formatted string representation of that number
+  const [integerPart, decimalPart] = number.toFixed(2).split("."); //call ensures that the number is rounded to 2 decimal places and returns a string and the split, splits the string into two parts integer part and decimal part
   let formattedIntegerPart = "";
 
   for (let i = 0; i < integerPart.length; i++) {
+    // The purpose of this loop and conditions is to format the integer part of the number by adding spaces every three digits
     if ((integerPart.length - i) % 3 === 0 && i !== 0) {
       formattedIntegerPart += " ";
     }
     formattedIntegerPart += integerPart[i];
   }
 
-  return `${formattedIntegerPart}.${decimalPart}`;
+  return `${formattedIntegerPart}.${decimalPart}`; //After the loop completes, the code returns a formatted string representation of the number by combining the formatted integer part and the decimal part
 }
-
+// format number function is used to format the balance amount.
 const leo = `${leoName} ${leoSurname} (Owed: R ${formatNumber(
-  Math.abs(parseFloat(leoBalance))
+  // a formatted string for Leo's information it includes his name, surname, and formatted balance amount.
+  Math.abs(parseFloat(leoBalance)) // function ensures that the balance value is treated as positive to prevent negative amounts from appearing in the formatted string.
 )})`;
 const sarah = `${sarahName} ${sarahSurname} (Owed: R ${formatNumber(
   Math.abs(parseFloat(sarahBalance))
 )})`;
 const totalText = `  Total amount owed: R ${formatNumber(Math.abs(owed))}\n`;
-const result = `\n${leo}\n${sarah}\n\n${divider}\n${totalText}${divider}`;
-
+const result = `\n${leo}\n${sarah}\n\n${divider}\n${totalText}${divider}`; //This creates the final result string by combining the formatted strings for Leo and Sarah
+//The ${} syntax is used to interpolate variables and function results into the string.
 console.log(result);
