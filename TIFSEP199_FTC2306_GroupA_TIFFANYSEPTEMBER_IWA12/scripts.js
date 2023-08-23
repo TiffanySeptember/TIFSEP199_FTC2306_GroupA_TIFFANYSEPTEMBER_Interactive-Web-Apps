76a3078ccc0ac9ca186a7bdbc28bf376035b42b4
TiffanySeptember <137154346+TiffanySeptember@users.noreply.github.com>
2023-08-23 18:04:47 +0200

    completed challenge

--- TIFSEP199_FTC2306_GroupA_TIFFANYSEPTEMBER_IWA12/scripts.js
+++ TIFSEP199_FTC2306_GroupA_TIFFANYSEPTEMBER_IWA12/scripts.js
@@ -1,39 +1,85 @@
-// Get references to the dl elements for each order
-const order1 = document.querySelector('[data-key="order1"]');
-const order2 = document.querySelector('[data-key="order2"]');
-const order3 = document.querySelector('[data-key="order3"]');
+const STATUS_MAP = {
+  shelf: {
+    color: "green",
+    canReserve: true,
+    canCheckout: true,
+    canCheckIn: false,
+  },
+  reserved: {
+    color: "blue",
+    canReserve: false,
+    canCheckout: true,
+    canCheckIn: false,
+  },
+  overdue: {
+    color: "red",
+    canReserve: false,
+    canCheckout: false,
+    canCheckIn: true,
+  },
+  checkedOut: {
+    color: "orange",
+    canReserve: false,
+    canCheckout: false,
+    canCheckIn: true,
+  },
+};
 
-// Extract the relevant elements within each order
-const biscuits1 = order1.querySelector(".biscuits .count");
-const donuts1 = order1.querySelector(".donuts .count");
-const pancakes1 = order1.querySelector(".pancakes .count");
-const status1 = order1.querySelector(".status dd");
+// Edit below line
 
-const biscuits2 = order2.querySelector(".biscuits .count");
-const donuts2 = order2.querySelector(".donuts .count");
-const pancakes2 = order2.querySelector(".pancakes .count");
-const status2 = order2.querySelector(".status dd");
+// For book1
+const book1 = document.querySelector("#book1");
+const reserveBook1 = book1.querySelector(".reserve");
+const checkoutBook1 = book1.querySelector(".checkout");
+const checkinBook1 = book1.querySelector(".checkin");
+const statusBook1 = book1.querySelector(".status");
 
-const biscuits3 = order3.querySelector(".biscuits .count");
-const donuts3 = order3.querySelector(".donuts .count");
-const pancakes3 = order3.querySelector(".pancakes .count");
-const status3 = order3.querySelector(".status dd");
+// For book2
+const book2 = document.querySelector("#book2");
+const reserveBook2 = book2.querySelector(".reserve");
+const checkoutBook2 = book2.querySelector(".checkout");
+const checkinBook2 = book2.querySelector(".checkin");
+const statusBook2 = book2.querySelector(".status");
 
-// Extract values from data attributes and update the DOM
-biscuits1.textContent = order1.dataset.biscuits;
-donuts1.textContent = order1.dataset.donuts;
-pancakes1.textContent = order1.dataset.pancakes;
-status1.textContent =
-  order1.dataset.delivered === "true" ? "Delivered" : "Pending";
+// For book3
+const book3 = document.querySelector("#book3");
+const reserveBook3 = book3.querySelector(".reserve");
+const checkoutBook3 = book3.querySelector(".checkout");
+const checkinBook3 = book3.querySelector(".checkin");
+const statusBook3 = book3.querySelector(".status");
 
-biscuits2.textContent = order2.dataset.biscuits;
-donuts2.textContent = order2.dataset.donuts;
-pancakes2.textContent = order2.dataset.pancakes;
-status2.textContent =
-  order2.dataset.delivered === "true" ? "Delivered" : "Pending";
+checkinBook1.style.color = "none";
+statusBook1.style.color = STATUS_MAP[statusBook1.textContent].color;
+reserveBook1.disabled = !STATUS_MAP[statusBook1.textContent].canReserve;
+checkoutBook1.disabled = !STATUS_MAP[statusBook1.textContent].canCheckout;
+checkinBook1.disabled = !STATUS_MAP[statusBook1.textContent].canCheckIn;
+reserveBook1.style.color = !STATUS_MAP[statusBook1.textContent].canReserve
+  ? "black"
+  : "none";
+checkoutBook1.style.color = !STATUS_MAP[statusBook1.textContent].canCheckout
+  ? "black"
+  : "none";
 
-biscuits3.textContent = order3.dataset.biscuits;
-donuts3.textContent = order3.dataset.donuts;
-pancakes3.textContent = order3.dataset.pancakes;
-status3.textContent =
-  order3.dataset.delivered === "true" ? "Delivered" : "Pending";
+checkinBook2.style.color = "none";
+statusBook2.style.color = STATUS_MAP[statusBook2.textContent].color;
+reserveBook2.disabled = !STATUS_MAP[statusBook2.textContent].canReserve;
+checkoutBook2.disabled = !STATUS_MAP[statusBook2.textContent].canCheckout;
+checkinBook2.disabled = !STATUS_MAP[statusBook2.textContent].canCheckIn;
+reserveBook2.style.color = !STATUS_MAP[statusBook1.textContent].canReserve
+  ? "black"
+  : "none";
+checkoutBook2.style.color = !STATUS_MAP[statusBook1.textContent].canCheckout
+  ? "black"
+  : "none";
+
+checkinBook3.style.color = "none";
+statusBook3.style.color = STATUS_MAP[statusBook3.textContent].color;
+reserveBook3.disabled = !STATUS_MAP[statusBook3.textContent].canReserve;
+checkoutBook3.disabled = !STATUS_MAP[statusBook3.textContent].canCheckout;
+checkinBook3.disabled = !STATUS_MAP[statusBook3.textContent].canCheckIn;
+reserveBook3.style.color = !STATUS_MAP[statusBook1.textContent].canReserve
+  ? "black"
+  : "none";
+checkoutBook3.style.color = !STATUS_MAP[statusBook1.textContent].canCheckout
+  ? "black"
+  : "none";
